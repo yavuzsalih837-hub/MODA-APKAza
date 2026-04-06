1b5bbd37e4af8bde73a4d7a84302e7b25b363ae8
yavuzsalih837-hub <yavuzsalih837@gmail.com>
2026-04-06 14:24:26 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,33 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { motion, AnimatePresence } from 'motion/react';
-import { Layout } from './components/Layout';
-import { Home } from './pages/Home';
-import { Categories } from './pages/Categories';
-import { CategoryDetail } from './pages/CategoryDetail';
-import { HasirKadinSapka } from './pages/HasirKadinSapka';
-import { SiperlikSapka } from './pages/SiperlikSapka';
-// import { Products } from './pages/Products';
-import { Contact } from './pages/Contact';
-// import { Admin } from './pages/Admin';
-import { ProductProvider } from './context/ProductContext';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from './Layout';
+import { Home } from './Home';
+import { Categories } from './Categories';
+import { CategoryDetail } from './CategoryDetail';
+import { HasirKadinSapka } from './HasirKadinSapka';
+import { SiperlikSapka } from './SiperlikSapka';
+import { Contact } from './Contact';
+import { ProductProvider } from './ProductContext';
+import { FloatingWhatsApp } from './FloatingWhatsApp';
 
-const AnimatedRoutes = () => {
-  const location = useLocation();
-  
-  return (
-    <AnimatePresence mode="wait">
-      <motion.div 
-        key={location.pathname}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        <Routes location={location}>
-          <Route path="/" element={<Home />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/categories/hasir-kadin-sapka" element={<HasirKadinSapka />} />
-          <Route path="/categories/siperlik-kadin-sapka" element={<SiperlikSapka />} />
-          <Route path="/categories/:slug" element={<CategoryDetail />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </motion.div>
-    </AnimatePresence>
-  );
-};
-
-export default function App() {
+function App() {
   return (
     <ProductProvider>
-      <Router>
+      <Router basename="/moda-sapka-yeni">
         <Layout>
-          <AnimatedRoutes />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/category/:id" element={<CategoryDetail />} />
+            <Route path="/hasir-kadin-sapka" element={<HasirKadinSapka />} />
+            <Route path="/siperlik-sapka" element={<SiperlikSapka />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </Layout>
+        <FloatingWhatsApp />
       </Router>
     </ProductProvider>
   );
 }
+
+export default App;
