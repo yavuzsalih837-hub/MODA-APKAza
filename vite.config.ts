844ec3989d4a1d62520fedb4b7d038ea4e8d16f0
yavuzsalih837-hub <yavuzsalih837@gmail.com>
2026-04-06 21:43:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base ayarı, GitHub Pages'deki alt klasör isminle aynı olmalıdır
-  base: '/moda-sapka-yeni/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  base: '/moda-sapka-yeni/', // Burası klasör adınla aynı olmalı
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 })
